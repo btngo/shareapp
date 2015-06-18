8a0992e55e76a45f1c5c7a092317570766cca110
BTNGO <con3cthanh@gmail.com>
2015-06-18 08:16:45 +0200

    WIP

--- www/js/app.js
+++ www/js/app.js
@@ -4,68 +4,84 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', [
+  'restangular',
+  'ionic',
+  'starter.controllers'
+])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+      .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'AppCtrl'
+      })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
-
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
+      .state('app.utils', {
+        url: "/utils",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/utils.html"
+          }
         }
-      }
-    })
+      })
 
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
+      .state('app.favorites', {
+        url: "/favorites",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/favorites.html"
+          }
+        }
+      })
+
+      .state('app.agenda', {
+        url: "/agenda",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/agenda.html",
+            controller: 'AgendaCtrl'
+          }
+        }
+      })
+
+      .state('app.agenda-detail', {
+        url: "/agenda/:eventId",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/agenda-detail.html",
+            controller: 'AgendaDetailCtrl'
+          }
+        }
+      })
+
+      .state('app.setting', {
+        url: "/setting",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/setting.html",
+            controller: 'SettingCtrl'
+          }
+        }
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/agenda');
+  })
+  .config(function(RestangularProvider) {
+    RestangularProvider.setFullResponse(true);
+  })
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       }
-    }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
