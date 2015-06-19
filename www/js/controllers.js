11d9ac927d9f6ae4a0d3d5d5d34c336a891ef0a3
Ba Tuan <btngo@linagora.com>
2015-06-19 19:04:02 +0200

    another change

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,7 +41,76 @@ angular.module('starter.controllers', [])
     };
   })
 
-  .controller('AgendaCtrl',['$scope', 'agendaService', '$http', function($scope, agendaService, $http) {
+  .controller('AgendaCtrl',function($scope, $ionicModal, agendaService, locationFavoriteService, $http) {
+
+    $scope.favorites = JSON.parse(window.localStorage['locations']) || [];
+    console.log('locations are:', $scope.favorites[0]);
+    $ionicModal.fromTemplateUrl('templates/addNewEvent.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.addNewEvent = function() {
+      $scope.modal.show();
+    };
+
+    $scope.createNewEvent = function(){
+      var req = {
+        method: 'POST',
+        url: '/api/events/',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: {"name":"Event 4", "location":"1", "owner":"0634124981", "event_date":"2015-05-17T00:00"}
+      };
+
+      $http(req).success(function(data, status) {
+        if(status === 201 ){
+          console.log('success create events');
+          console.log(' events', data);
+        }
+      }).error(function(data, status, headers) {
+        console.log('error create events');
+      });
+    };
+    Number.prototype.toHHMMSS = function () {
+      var hours   = Math.floor(this / 3600);
+      var minutes = Math.floor((this - (hours * 3600)) / 60);
+      if (hours   < 10) {hours   = "0"+hours;}
+      if (minutes < 10) {minutes = "0"+minutes;}
+      var time    = hours+':'+minutes+':00';
+      return time;
+    };
+
+    Date.prototype.formatMMDDYYYY = function(){
+      return this.getDate() +
+        "/" +  ((this.getMonth() + 1)  < 10 ? "0" + (this.getMonth() + 1):(this.getMonth() + 1)) +
+        "/" +  this.getFullYear();
+    };
+    $scope.currentDate = new Date();
+    $scope.dateShortFormat = $scope.currentDate.formatMMDDYYYY();
+
+    $scope.datePickerCallback = function (val) {
+      if(typeof(val)==='undefined'){
+        console.log('Date not selected');
+      }else{
+        $scope.dateShortFormat = val.formatMMDDYYYY();
+        console.log('Selected date is : ', val);
+      }
+    };
+
+    $scope.slots = {epochTime: 12600, format: 12, step: 15};
+
+    $scope.timepicker =  $scope.slots.epochTime.toHHMMSS();
+    $scope.timePickerCallback = function (val) {
+      if (typeof (val) === 'undefined') {
+        console.log('Time not selected');
+      } else {
+        $scope.timepicker = val.toHHMMSS();
+        console.log('Selected time is : ', val);    // `val` will contain the selected time in epoch
+      }
+    };
 
     var req = {
       method: 'GET',
@@ -57,15 +126,14 @@ angular.module('starter.controllers', [])
       $scope.events = data;
       console.log(data);
       agendaService.setList(data);
-    }).
-      error(function(data, status, headers, config) {
+    }).error(function(data, status, headers, config) {
         console.log('error get events');
         console.log(' events', data);
         console.log(' events', status);
         console.log(' events', headers);
       });
 
-  }])
+  })
 
 
   .controller('AgendaDetailCtrl', function($scope, agendaService, $stateParams) {
@@ -73,7 +141,7 @@ angular.module('starter.controllers', [])
 
   })
 
-  .controller('LocationCtrl',['$scope', 'locationService', '$http', function($scope, locationService, $http) {
+  .controller('LocationCtrl',['$scope', 'locationService', 'locationFavoriteService', '$http', function($scope, locationService, locationFavoriteService, $http) {
 
     var req = {
       method: 'GET',
@@ -83,13 +151,14 @@ angular.module('starter.controllers', [])
       }
     };
 
-    $http(req).success(function(data, status, headers, config) {
+    $http(req).success(function(data) {
       console.log('success get locations');
       $scope.locations = data;
       console.log(data);
+      window.localStorage['locations'] = JSON.stringify(data);
       locationService.setList(data);
-    }).
-      error(function(data, status, headers, config) {
+      locationFavoriteService.setList(data);
+    }).error(function() {
         console.log('error get locations');
       });
 
@@ -103,7 +172,7 @@ angular.module('starter.controllers', [])
   .controller('SettingCtrl', function($scope, $stateParams) {
   })
 
-  .factory('locationService', function(Restangular) {
+  .factory('locationService', function() {
     var locations =[];
 
     function getList(){
@@ -124,12 +193,34 @@ angular.module('starter.controllers', [])
       getList: getList,
       setList: setList,
       getLocation: getLocation
-
     }
-
   })
 
-  .factory('agendaService', function(Restangular) {
+  .factory('locationFavoriteService', function() {
+    var locations =[];
+
+    function getList(){
+      return locations;
+    }
+    function setList(locationList){
+      locations = locationList;
+    }
+    function getLocation(locationId){
+      var filteredLocation =  locations.filter(function (location) {
+        var bool = Number(location.location_id) === Number(locationId);
+        return bool;
+      });
+      console.log('event filtered', filteredLocation.length);
+      return filteredLocation[0];
+    }
+    return {
+      getList: getList,
+      setList: setList,
+      getLocation: getLocation
+    }
+  })
+
+  .factory('agendaService', function() {
     var events =[];
 
     function getList(){
@@ -155,9 +246,6 @@ angular.module('starter.controllers', [])
       getList: getList,
       setList: setList,
       getEvent: getEvent
-
     }
-
-  })
-;
+  });
 
